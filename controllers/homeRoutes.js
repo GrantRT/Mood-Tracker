009864fb2870a1e2cf40d4127ee3fb965a6edc9c
GrantRT <granto_92@hotmail.com>
2022-04-05 21:28:27 +0100

    setup handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,25 @@
-const router = require("express").Router();
-const { User, Mood } = require("../../models");
+const router = require('express').Router();
+// const { User, Mood } = require('../../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-        
-    }
+  try {
+    res.render('homepage', {
+      //   logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
