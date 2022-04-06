671c3190ef48f2e24b71f632283ef93618110943
LisaCR01 <lisagunn01@gmail.com>
2022-04-06 20:42:12 +0100

    made login.js only relevent content so that sign in and login seperate pages

--- public/js/login.js
+++ public/js/login.js
@@ -22,31 +22,6 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
 
-  // Collect values from the signin form
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (username && email && password) {
-    // Send a POST request to the API endpoint
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ username, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    // If successful, redirect the browser to the home page
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
