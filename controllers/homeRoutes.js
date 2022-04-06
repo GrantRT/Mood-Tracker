fc1fe8def8a91f75d79e62e8a2a1fe99de77abca
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-04-06 21:30:13 +0100

    update route for log out

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,35 +1,45 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // const { User, Mood } = require('../../models');
-const withAuth = require('../utils/auth');
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.render('homepage', {
-        logged_in: req.session.logged_in,
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to the homepage
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
+router.get("/logout", (req, res) => {
   // If the user is already logged in, redirect the request to the homepage
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('signup');
+  res.render("logout");
+});
+
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
 });
 
 module.exports = router;
