f8749a599261b5fe3b4a7e9ede6682a099dbab97
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-04-07 18:18:31 +0100

    adding route for dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,19 +42,10 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/dashboard", withAuth, async (req, res) => {
+router.get("/dashboard", async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: mood }],
-    });
-
-    const user = userData.get({ plain: true });
-
     res.render("dashboard", {
-      ...user,
-      logged_in: true,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
