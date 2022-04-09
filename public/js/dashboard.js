81fe365c609f6b12a01c4c862b1c97c0f572358d
LisaCR01 <lisagunn01@gmail.com>
2022-04-09 11:45:26 +0100

    added styling

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -107,22 +107,5 @@ const renderWeeklyChart = () => {
   const moodChart = new Chart(document.getElementById('moodChart'), config);
 };
 
-function findOutputForSlider(el) {
-  var idVal = el.id;
-  outputs = document.getElementsByTagName('output');
-  for (var i = 0; i < outputs.length; i++) {
-    if (outputs[i].htmlFor == idVal) return outputs[i];
-  }
-}
-
-var sliders = document.querySelectorAll('input[type="range"].slider');
-[].forEach.call(sliders, function (slider) {
-  var output = findOutputForSlider(slider);
-  if (output) {
-    slider.addEventListener('input', function (event) {
-      output.value = event.target.value;
-    });
-  }
-});
 
 // get mood data
